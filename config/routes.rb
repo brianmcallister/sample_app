bffdf0e9a2d9b4c841e75959b34a16147705ffbe
Brian McAllister <brian@brianmcallister.com>
2014-08-20 23:53:02 -0400

    Add a users controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleApp::Application.routes.draw do
-  get "user/new"
+  get 'users/new'
+  
   root  'static_pages#home'
+  get '/signup',  to: 'users#new'
   get '/help',    to: 'static_pages#help'   
   get '/about',   to: 'static_pages#about'  
   get '/contact', to: 'static_pages#contact'
