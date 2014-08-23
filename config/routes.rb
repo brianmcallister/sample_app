f12e439dc9192f864607dd29d366133701c05635
Brian McAllister <brian@brianmcallister.com>
2014-08-23 11:43:30 -0400

    Wire up the sign up form.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 SampleApp::Application.routes.draw do
-  get 'users/new'
-  
   root  'static_pages#home'
   get '/signup',  to: 'users#new'
   get '/help',    to: 'static_pages#help'   
   get '/about',   to: 'static_pages#about'  
   get '/contact', to: 'static_pages#contact'
+  
+  resources :users
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
